c197cc63c090e60c638d0a241fdda87b94fa4807
teaaahyung <mptan18@gmail.com>
2018-11-01 19:44:51 -0400

    Added program

---
+++ untitled0.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 31 21:59:51 2018
+
+@author: mtan
+"""
+
+for i in range(10):
+    print("GCI is great")
+    
+name = input("Enter your name: ")
+print("Hello " + name + ", pleased to meet you! "
+      "Did you know that your name backwards is " + name[::-1] + "?")
+
